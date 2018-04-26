9843cc0349561e5f84dae6564c397ef4a78cae7e
Fminorelli <felipeminorelli@gmail.com>
2018-04-26 17:42:55 -0300

    bug Life

--- src/game.js
+++ src/game.js
@@ -159,7 +159,7 @@ function create() {
     upgradeSound=game.add.audio('upgrade')
     hitp = game.add.audio('hitp')
     hitp.volume -= 0.3
-    lifeSound-=0.5
+    lifeSound.volume-=0.5
     
     morriSound=game.add.audio('morri')
     faliceuSound=game.add.audio('faliceu')
